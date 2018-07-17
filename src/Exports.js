b6c63829fe551d92696116b0c963beaa0cfb46e0
Luca Forni <l.forni@modalsource.com>
2018-07-17 14:08:35 +0200

    rimodulati gli export

--- src/Exports.js
+++ src/Exports.js
@@ -1,9 +1,4 @@
-import DnDSource from './DnDSource'
-import DnDContext from './DnDContext'
-import AddMorePopover from './AddMorePopover'
 import ViewTypes from './ViewTypes'
 import SummaryPos from './SummaryPos'
-import SchedulerData from './SchedulerData'
-import DemoData from './DemoData'
 
-export {SchedulerData, ViewTypes, SummaryPos, DnDSource, DnDContext, AddMorePopover, DemoData}
+export { ViewTypes, SummaryPos}
